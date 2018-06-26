998e506fef7cee1dd6ca0e245c8d89b91fa1421b
Unknown <eric@sigmalis.com>
2018-06-26 08:26:31 +0200

    Add version

--- src/PastellClient.php
+++ src/PastellClient.php
@@ -4,16 +4,40 @@ namespace Libriciel;
 
 class PastellClient {
 
+	private $pastell_url;
 	private $login;
 	private $password;
 
-	public function __construct($login,$password) {
+	public function __construct($pastell_url,$login,$password) {
+		$this->pastell_url = $pastell_url;
 		$this->login = $login;
 		$this->password = $password;
 	}
 
+	/**
+	 * @return string
+	 * @throws \GuzzleHttp\Exception\GuzzleException
+	 */
 	public function version(){
-		return "Not implemented";
+		//TODO gérer les logs en PSR3
+
+		// TODO injection de la dépendance
+		$client = new \GuzzleHttp\Client();
+
+		// TODO gérer les exceptions, le timeout
+		$res = $client->request(
+			'GET',
+			$this->pastell_url."/api/v2/Version", //TODO constante magique
+			[
+				'auth' => [
+					$this->login,
+					$this->password
+				]
+			]
+		);
+
+		// TODO gérer plus de retour
+		return $res->getBody()->getContents();
 	}
 
 
